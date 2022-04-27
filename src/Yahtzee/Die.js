9f3129ee0f4efef9d49020e3d85a923d0d98123e
Savanah Steigen <savanahsteigen@gmail.com>
2022-04-27 18:12:42 -0400

    add yahtzee example

--- src/Yahtzee/Die.js
+++ src/Yahtzee/Die.js
@@ -1,16 +1,68 @@
 import React, { Component } from "react";
 import "./Die.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDiceOne,
+  faDiceTwo,
+  faDiceThree,
+  faDiceFour,
+  faDiceFive,
+  faDiceSix,
+} from "@fortawesome/free-solid-svg-icons";
+
 class Die extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  getDiceIcon(num) {
+    let icon;
+    switch (num) {
+      case 1:
+        icon = faDiceOne;
+        break;
+      case 2:
+        icon = faDiceTwo;
+        break;
+      case 3:
+        icon = faDiceThree;
+        break;
+      case 4:
+        icon = faDiceFour;
+        break;
+      case 5:
+        icon = faDiceFive;
+        break;
+      case 6:
+        icon = faDiceSix;
+        break;
+      default:
+        break;
+    }
+
+    return icon;
+  }
+
+  handleClick() {
+    this.props.handleClick(this.props.idx);
+  }
+
   render() {
+    const { val, locked, rolling, disabled } = this.props;
+
+    const icon = this.getDiceIcon(val);
+
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
-      >
-        {this.props.val}
-      </button>
+      <FontAwesomeIcon disabled={disabled}
+        className={`Die ${locked ? "Die-locked" : ""} ${
+          rolling ? "Die-rolling" : ""
+        }`}
+        icon={icon}
+        onClick={this.handleClick}
+      />
     );
   }
 }
--- src/Yahtzee/Game.js
+++ src/Yahtzee/Game.js
@@ -10,9 +10,10 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dice: Array.from({ length: NUM_DICE }),
+      dice: Array.from({ length: NUM_DICE }).map((d) => 1),
       locked: Array(NUM_DICE).fill(false),
       rollsLeft: NUM_ROLLS,
+      rolling: false,
       scores: {
         ones: undefined,
         twos: undefined,
@@ -26,69 +27,107 @@ class Game extends Component {
         smallStraight: undefined,
         largeStraight: undefined,
         yahtzee: undefined,
-        chance: undefined
-      }
+        chance: undefined,
+      },
     };
     this.roll = this.roll.bind(this);
     this.doScore = this.doScore.bind(this);
+    this.toggleLocked = this.toggleLocked.bind(this);
+    this.animateRoll = this.animateRoll.bind(this);
+  }
+
+  componentDidMount() {
+    this.animateRoll();
+  }
+
+  animateRoll() {
+    this.setState({ rolling: true }, () => {
+      setTimeout(this.roll, 1000);
+    });
   }
 
   roll(evt) {
     // roll dice whose indexes are in reroll
-    this.setState(st => ({
+    this.setState((st) => ({
       dice: st.dice.map((d, i) =>
         st.locked[i] ? d : Math.ceil(Math.random() * 6)
       ),
       locked: st.rollsLeft > 1 ? st.locked : Array(NUM_DICE).fill(true),
-      rollsLeft: st.rollsLeft - 1
+      rollsLeft: st.rollsLeft - 1,
+      rolling: false,
     }));
   }
 
   toggleLocked(idx) {
+    // If there are no rolls left, or the dice are currenly rolling, do nothing.
+    if (this.state.rollsLeft === 0 || this.state.rolling) {
+      return;
+    }
+
     // toggle whether idx is in locked or not
-    this.setState(st => ({
+    this.setState((st) => ({
       locked: [
         ...st.locked.slice(0, idx),
         !st.locked[idx],
-        ...st.locked.slice(idx + 1)
-      ]
+        ...st.locked.slice(idx + 1),
+      ],
     }));
   }
 
   doScore(rulename, ruleFn) {
     // evaluate this ruleFn with the dice and score this rulename
-    this.setState(st => ({
+    this.setState((st) => ({
       scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
       rollsLeft: NUM_ROLLS,
-      locked: Array(NUM_DICE).fill(false)
+      locked: Array(NUM_DICE).fill(false),
     }));
-    this.roll();
+
+    this.animateRoll();
+  }
+
+  displayRollInfo() {
+    const messages = [
+      "0 Rolls Left",
+      "1 Roll Left",
+      "2 Rolls Left",
+      "Starting Round",
+    ];
+
+    return messages[this.state.rollsLeft];
   }
 
   render() {
-    return (
-      <div className='Game'>
-        <header className='Game-header'>
-          <h1 className='App-title'>Yahtzee!</h1>
+    const {dice, locked, rollsLeft, rolling, scores} = this.state;
 
-          <section className='Game-dice-section'>
+    return (
+      <div className="Game">
+        <header className="Game-header">
+          <h1 className="Game-title">Yahtzee!</h1>
+
+          <section className="Game-dice-section">
             <Dice
-              dice={this.state.dice}
-              locked={this.state.locked}
+              dice={dice}
+              locked={locked}
               handleClick={this.toggleLocked}
+              disabled={!rollsLeft}
+              rolling={rolling}
             />
-            <div className='Game-button-wrapper'>
+            <div className="Game-button-wrapper">
               <button
-                className='Game-reroll'
-                disabled={this.state.locked.every(x => x)}
-                onClick={this.roll}
+                className="Game-reroll"
+                disabled={
+                  locked.every((x) => x) ||
+                  rollsLeft === 0 ||
+                  rolling
+                }
+                onClick={this.animateRoll}
               >
-                {this.state.rollsLeft} Rerolls Left
+                {this.displayRollInfo()}
               </button>
             </div>
           </section>
         </header>
-        <ScoreTable doScore={this.doScore} scores={this.state.scores} />
+        <ScoreTable doScore={this.doScore} scores={scores} />
       </div>
     );
   }
