f0bb4738be1be77e8a0c9469115acd5e969c4fcb
Savanah Steigen <savanahsteigen@gmail.com>
2023-01-22 13:10:37 -0500

    Refactor button list to use hooks

--- src/ButtonList/ButtonList.js
+++ src/ButtonList/ButtonList.js
@@ -1,41 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { choose } from "helpers";
 
 import './ButtonList.css';
 
-class ButtonList extends Component {
-  static defaultProps = {
-    colors: ["#e056fd", "#eb4d4b", "#badc58", "#f0932b"],
-  };
+export default function ButtonList(props) {
+  const colors = ["#e056fd", "#eb4d4b", "#badc58", "#f0932b"];
 
-  constructor(props) {
-    super(props);
-    this.state = { color: choose(this.props.colors) };
-  }
+  const [color, setColor] = useState(choose(colors));
 
-  changeColor(newColor) {
-    this.setState({ color: newColor });
-  }
-
-  render() {
-    return (
-      <div className="ButtonList" style={{ backgroundColor: this.state.color }}>
-        {this.props.colors.map((color, index) => {
-          return (
-            <button
-              key={index}
-              style={{ backgroundColor: color }}
-              onClick={() => this.changeColor(color)}
-              // onClick={this.changeColor.bind(this, color)}
-              // Binding in the render function is not ideal!
-            >
-              Click on me!
-            </button>
-          );
-        })}
-      </div>
-    );
-  }
+  return (
+    <div className="ButtonList" style={{ backgroundColor: color }}>
+      {colors.map((color, index) => {
+        return (
+          <button
+            key={index}
+            style={{ backgroundColor: color }}
+            // Binding in the render function is not ideal!
+            onClick={() => setColor(color)}
+          >
+            Click on me!
+          </button>
+        );
+      })}
+    </div>
+  );
 }
-
-export default ButtonList;
