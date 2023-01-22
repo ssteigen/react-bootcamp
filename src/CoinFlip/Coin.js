e115181ee76156da0d4b9e6ffebb8e2a911dee17
Savanah Steigen <savanahsteigen@gmail.com>
2023-01-22 13:07:41 -0500

    Reactor coin flip to use hooks

--- src/CoinFlip/Coin.js
+++ src/CoinFlip/Coin.js
@@ -1,23 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Coin.css";
 
 import imgHeads from "./cent-head.png";
 import imgTails from "./cent-tail.png";
 
-class Coin extends Component {
-  render() {
-    return (
-      <div className={`Coin ${this.props.side} ${this.props.flipping ? 'flipping' : ''}`}>
-        <div className='side-a'>
-          <img src={imgHeads} alt='heads' />
-        </div>
-        <div className='side-b'>
-          <img src={imgTails} alt='tails' />
-        </div>
+export default function Coin({side, flipping}) {
+  return (
+    <div className={`Coin ${side} ${flipping ? 'flipping' : ''}`}>
+      <div className='side-a'>
+        <img src={imgHeads} alt='heads' />
       </div>
-
-    );
-  }
-}
-
-export default Coin;
+      <div className='side-b'>
+        <img src={imgTails} alt='tails' />
+      </div>
+    </div>
+  );
+};
--- src/CoinFlip/Coinflip.js
+++ src/CoinFlip/Coinflip.js
@@ -1,80 +1,46 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Coinflip.css";
 import Coin from "./Coin";
 import { choose } from "../helpers";
 import imgHeads from "./cent-head.png";
 import imgTails from "./cent-tail.png";
 
-class Coinflip extends Component {
-  static defaultProps = {
-    sides: ["heads", "tails"],
-    coins: [
-      { side: "heads", imgSrc: imgHeads },
-      { side: "tails", imgSrc: imgTails },
-    ],
-    flipping: false,
-  };
+export default function Coinflip({ title = "Let's flip a coin!", className }) {
+  const coins = [{ side: "heads", imgSrc: imgHeads }, { side: "tails", imgSrc: imgTails }];
+  const sides = coins.map(coin => coin.side);
 
-  constructor(props) {
-    super(props);
+  const [flipping, setFlipping] = useState(false);
+  const [side, setSide] = useState("heads");
+  const [counters, setCounters] = useState({ numFlips: 0, numHeads: 0, numTails: 0 });
 
-    this.state = {
-      side: "heads",
-      currCoin: null,
-      numFlips: 0,
-      numHeads: 0,
-      numTails: 0,
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  // Flip the coin
-  flipCoin() {
-    const newSide = choose(this.props.sides);
-
-    this.setState((st) => {
-      return {
-        ...st,
-        side: newSide,
-        flipping: true,
-      };
-    });
+  function flipCoin() {
+    setFlipping(true);
+    const newSide = choose(sides);
+    setSide(newSide);
 
     setTimeout(() => {
-      this.setState((st) => {
-        return {
-          ...st,
-          numFlips: st.numFlips + 1,
-          numHeads: st.numHeads + (newSide === "heads" ? 1 : 0),
-          numTails: st.numTails + (newSide === "tails" ? 1 : 0),
-          flipping: false,
-        };
+      setCounters({
+        numFlips: counters.numFlips + 1,
+        numHeads: counters.numHeads + (newSide === "heads" ? 1 : 0),
+        numTails: counters.numTails + (newSide === "tails" ? 1 : 0)
       });
+      setFlipping(false);
     }, 2000);
   }
 
-  handleClick() {
-    this.flipCoin();
-  }
-
-  render() {
-    return (
-      <div className={`CoinFlip ${this.props.className} bg-gradient`}>
-        <h1 className="CoinFlip-title">Let's flip a coin!</h1>
-        <div className="CoinFlip-content">
-          <Coin side={this.state.side} flipping={this.state.flipping} />
-          <p>
-            Out of {this.state.numFlips} flips, there have been{" "}
-            {this.state.numHeads} heads and {this.state.numTails} tails.
-          </p>
-        </div>
-        <button onClick={this.handleClick} disabled={this.state.flipping}>
-          {this.state.flipping ? "Flipping" : "Flip Coin"}
-        </button>
+  return (
+    <div className={`CoinFlip ${className} bg-gradient`}>
+      <h1 className="CoinFlip-title">{title}</h1>
+      <div className="CoinFlip-content">
+        <Coin side={side} flipping={flipping} />
+        <p>
+          Out of {counters.numFlips} {counters.numFlips === 1 ? "flip" : "flips"}, there have been{" "}
+          {counters.numHeads} heads and {counters.numTails} tails.
+        </p>
       </div>
-    );
-  }
+      <button onClick={flipCoin} disabled={flipping}>
+        {flipping ? "Flipping" : "Flip Coin"}
+      </button>
+    </div>
+  );
 }
-
-export default Coinflip;
